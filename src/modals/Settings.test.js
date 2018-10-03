490299cedf7c5d4be0a927da2a1e212991833fbb
Aaron <aaron4uk@gmail.com>
2018-10-03 11:23:12 -0500

    saving a link for reference

--- src/modals/Settings.test.js
+++ src/modals/Settings.test.js
@@ -4,6 +4,8 @@ import Adapter from 'enzyme-adapter-react-16'
 import Settings from './Settings'
 configure({ adapter: new Adapter() })
 
+//https://medium.com/@newyork.anthonyng/testing-react-components-state-b57bfc712b90
+
 it('opens the settings modal', () => {
   const settings = shallow(<Settings />)
   const openButton = settings.find('Button').at(0);
