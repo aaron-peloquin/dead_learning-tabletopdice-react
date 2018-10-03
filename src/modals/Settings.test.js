74d5a75fedf2dae388320870642d34818f32c648
Aaron <aaron4uk@gmail.com>
2018-10-03 18:33:01 -0500

    got shallow unit test working

--- src/modals/Settings.test.js
+++ src/modals/Settings.test.js
@@ -1,4 +1,4 @@
-// import React from 'react';
+import React from 'react';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16'
 import Settings from './Settings'
@@ -6,16 +6,8 @@ configure({ adapter: new Adapter() })
 
 //https://medium.com/@newyork.anthonyng/testing-react-components-state-b57bfc712b90
 
-it('opens the settings modal', () => {
-  const settings = shallow(<Settings />)
-  settings.debug()
-  // const openButton = settings.find('Button');
-  // settings.debug()
-  // openButton.debug()
-  // openButton.simulate('click')
-
-  // let settigns_inst = settings.instance()
-  // expect(settigns_inst.state.open).toEqual(false)
-  // settings.simulate('click')
+it("renders without errors", () => {
+  const element = shallow(<Settings />)
+  expect(element.length).toEqual(1)
 })
 
