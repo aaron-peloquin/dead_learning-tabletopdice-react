7e923dadd9d9d99a2abb7c2185b6e2ec578ce299
Aaron <aaron4uk@gmail.com>
2018-10-04 23:33:00 -0500

    moved loading it's localStorage state into this reducer on each update

--- src/storeReducers/sets.js
+++ src/storeReducers/sets.js
@@ -1,10 +1,33 @@
-import { saveToStorage } from './localStorage'
+import {
+  saveToStorage,
+  loadFromStorage
+} from './helper-localStorage'
 
+/**
+ * Reducer for {}.sets
+ * Attempts to pull state out of localStorage
+ * @param {array} state An array of objects
+ * @param {str} type The type of action to take on this dataset
+ * @param {str} paypload The data to update this state with
+ */
 export default function(state=[], { type, payload }) {
-  if(type === "addSet") {
-    state.push(payload)
+  let updateLocalStorage = false
+
+  const storageSets = loadFromStorage('sets')
+  if(storageSets) {
+    state = storageSets
   }
 
-  saveToStorage('sets', state)
+  switch (type) {
+    case "addSet":
+      updateLocalStorage = true
+      state.push(payload)
+      break;
+  }
+
+  /** Attempt to pass new state to localStorage as a JSON object */
+  if(updateLocalStorage){
+    saveToStorage('sets', state)
+  }
   return state  
 }
