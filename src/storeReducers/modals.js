352d3d4fdc8d5c8f4d9caae93ca772a6db4cc662
Aaron <aaron4uk@gmail.com>
2018-10-05 00:12:21 -0500

    created modals reducer for redux

---
+++ src/storeReducers/modals.js
@@ -0,0 +1,25 @@
+import {
+  TOGGLE_MODAL
+} from './../storeActions/toggleModal'
+
+/**
+ * Reducer for {}.modals
+ * @param {array} state An object of each modal,
+ *  each with a default boolean value of false
+ * @param {str} type The type of action to take on this dataset
+ * @param {str} paypload The data to update this state with
+ */
+
+const defaultState = {
+  settings: false,
+  addSet: false
+}
+
+export default function(state=defaultState, { type, payload }) {
+  switch (type) {
+    case TOGGLE_MODAL:
+      state[payload] = !state[payload]
+      break;
+  }
+  return state  
+}
