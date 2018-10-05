085cbd7ce94d1aa4cd5fafca9b510f49a883bd19
Aaron <aaron4uk@gmail.com>
2018-10-04 23:27:32 -0500

    moved default settings into the settings reducer, made it check localStorage every time it updates a setting to avoid multi-tab conflicts

--- src/storeReducers/settings.js
+++ src/storeReducers/settings.js
@@ -1,9 +1,34 @@
-import { saveToStorage } from './localStorage'
+import {
+  saveToStorage,
+  loadFromStorage,
+} from './helper-localStorage'
 
-export default function(state='', { type, payload }) {
+const defaultState = {
+  "showStandardArray": true,
+  "showStandardDie": {
+    "4": true,
+    "6": true,
+    "8": true,
+    "10": true,
+    "12": true,
+    "20": true,
+  },
+}
+
+export default function(state=defaultState, { type, payload }) {
+  let updateLocalStorage = false
+  const settingsStorage = loadFromStorage('settings')
+  if(settingsStorage) {
+    state = settingsStorage
+  }
+
   if(type === "updateSettings") {
+    updateLocalStorage = true
     state = payload
   }
-  saveToStorage('settings', state)
+
+  if(updateLocalStorage){
+    saveToStorage('settings', state)
+  }
   return state  
 }
