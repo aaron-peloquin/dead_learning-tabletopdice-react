b73abd5afba2208b2866e33c9f3ad6343da80f1b
Aaron <aaron4uk@gmail.com>
2018-10-06 20:25:18 -0500

    created placeholder element

---
+++ src/elements/AddNewSet.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {
+  Typography,
+  Grid,
+} from '@material-ui/core/'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+
+const AddNewSet = (props, state) => {
+console.log("props:  ", typeof props, props)
+console.log("state:  ", typeof state, state)
+}
+
+export default AddNewSet
