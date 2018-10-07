7b212a73c8918c05b684d6a2607dc5a1ffda054f
Aaron <aaron4uk@gmail.com>
2018-10-06 20:25:18 -0500

    created placeholder element

--- src/elements/AddNewSet.js
+++ src/elements/AddNewSet.js
@@ -1,55 +1,13 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import {
-  Button,
+  Typography,
+  Grid,
 } from '@material-ui/core/'
-import AddIcon from '@material-ui/icons/Add'
-import addSet from './../storeDispatchers/addSet'
-import { connect } from 'react-redux'
-import { withStyles } from '@material-ui/core/styles'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 
-const style = theme => {
-  return {
-    fabWrapper: {
-      position: "fixed",
-      right: theme.spacing.unit * 2,
-      bottom: theme.spacing.unit * 4,
-    },
-  }
+const AddNewSet = (props, state) => {
+console.log("props:  ", typeof props, props)
+console.log("state:  ", typeof state, state)
 }
 
-let AddNewSet = (props) => {
-  const { classes, activlyEditing } = props
-  if(activlyEditing) {
-    return <Fragment />
-  }
-
-  return <Fragment>
-    <div className={classes.fabWrapper}>
-      <Button
-        aria-label="Create new dice set"
-        onClick={addSet}
-        variant="fab"
-        color="primary"
-      >
-        <AddIcon />
-      </Button>
-    </div>
-  </Fragment>
-}
-
-const mapStateToProps = (state, props) => {
-  const { setEditStatus = [] } = state
-  let editing = false
-  if(setEditStatus.length>0){
-    editing = setEditStatus.some((val)=>{
-      return val===true
-    })
-  }
-
-  return {
-    activlyEditing: editing
-  }
-}
-
-AddNewSet = withStyles(style)(AddNewSet)
-export default connect(mapStateToProps)(AddNewSet)
+export default AddNewSet
