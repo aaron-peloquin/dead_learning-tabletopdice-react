e9ce4795d1fb3ed4afbc027a7bf2f2f388bbb821
Aaron <aaron4uk@gmail.com>
2018-10-06 23:49:04 -0500

    initial commit

--- src/elements/SetCards.js
+++ src/elements/SetCards.js
@@ -1,45 +1,41 @@
 import React, { Fragment } from 'react'
-import { Grid } from '@material-ui/core/'
+import {
+  Card,
+  Grid,
+  Paper,
+  Typography,
+  Button,
+} from '@material-ui/core/'
 import { connect } from 'react-redux'
-import { withStyles } from '@material-ui/core/styles'
-import SetCard from './SetCard'
 
-const style = theme => {
-  return {
-    gridContainer: {
-      width: '95%',
-      margin: 0,
-      paddingTop: theme.spacing.unit * 4,
-      paddingLeft: theme.spacing.unit * 2,
-      paddingRight: theme.spacing.unit * 8,
-    }
-  }
-}
+const fakeData = [
+  {name: "Short Sword", primary: "1d20+5", secondary: "1d6+3", note: "A short sword"},
+  {name: "Spear", primary: "1d20+5", secondary: "1d6+3", note: "A wooden spear"},
+  {name: "Dagger", primary: "1d20+5", secondary: "1d4+3", note: "A sly dagger"},
+  {name: "Short Sword", primary: "1d20+5", secondary: "1d6+3", note: "A short sword"},
+  {name: "Spear", primary: "1d20+5", secondary: "1d6+3", note: "A wooden spear"},
+]
 
 let SetCards = (props) => {
-  const { classes } = props
-  const sets = props.sets.slice()
-  let i = 0
   return <Fragment>
-    <Grid container justify="center" className={classes.gridContainer} spacing={24}>
-      {sets.sort((a,b)=>{
-        return a.name>b.name
-      }).map((setData) => {
-          i++
-          return <Grid key={i} item xs={12} sm={6} md={4} xl={3}>
-            <SetCard setData={setData} />
-          </Grid>
-        }
-      )}
-      </Grid>
-    </Fragment>
+    <Grid container justify="center" spacing={24}>
+      {fakeData.map(dataObj => <Grid item xs={5}>
+        <Card>
+          <Paper>
+            <Typography variant="headline">{dataObj.name}</Typography>
+            <Typography variant="subheading">{dataObj.note}</Typography>
+            <Button variant="contained" color="secondary">Roll</Button>
+          </Paper>
+        </Card>
+      </Grid>)}
+    </Grid>
+  </Fragment>
 }
 
 const mapStateToProps = (state, props) => {
   return {
-    sets: state.sets,
+    status: state.sets
   }
 }
 
-SetCards = withStyles(style)(SetCards)
 export default connect(mapStateToProps)(SetCards)
