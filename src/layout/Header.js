f718106b668579c2a6ea67325f1d285b1c4b9a9a
Aaron <aaron4uk@gmail.com>
2018-10-07 08:04:13 -0500

    adjusting columns for iPhone 5

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -11,10 +11,10 @@ function Header() {
   return <AppBar position="static" color="primary">
       <Toolbar>
         <Grid container>
-          <Grid item xs={5}>
+          <Grid item xs={8}>
             <Typography variant="headline" color="textSecondary" align="left">Tabletop Dice</Typography>
           </Grid>
-          <Grid item xs={7} align="right">
+          <Grid item xs={4} align="right">
           <a href="https://goo.gl/forms/HIDWZP4VsafmbQTA2" target="_blank" rel="noopener noreferrer"><Button variant="contained">Feedback</Button></a>
           </Grid>
         </Grid>
